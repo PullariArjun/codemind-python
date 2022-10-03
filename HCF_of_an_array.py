a67567f025ab8bc79ec28d7468374c3a42026bc6
PullariArjun <98091330+PullariArjun@users.noreply.github.com>
2022-10-03 21:33:59 +0530

    9n=int(input()) arr=[int(i) for i in input().split()] for i in range(arr[0],0,-1):     count=0     for j in arr:         if(j%i==0):             count+=1     if(count==len(arr)):         print(i)         break

---
+++ HCF_of_an_array.py
@@ -0,0 +1,10 @@
+9n=int(input())
+arr=[int(i) for i in input().split()]
+for i in range(arr[0],0,-1):
+    count=0
+    for j in arr:
+        if(j%i==0):
+            count+=1
+    if(count==len(arr)):
+        print(i)
+        break
