997068d1b9086b99c25d0fe4670e8490aaca75bc
PullariArjun <98091330+PullariArjun@users.noreply.github.com>
2022-11-28 15:31:09 +0530

    for _ in range(int(input())):     n,a,b,k=map(int,input().split())     c=0     for i in range(min(a,b),n+1):         if(i%a==0 and i%b==0):             continue         if(i%a==0 or i%b==0):             c+=1         if(c>=k):             print("Win")             break     else:         print("Lose")

---
+++ Appy_and_Contest.py
@@ -0,0 +1,13 @@
+for _ in range(int(input())):
+    n,a,b,k=map(int,input().split())
+    c=0
+    for i in range(min(a,b),n+1):
+        if(i%a==0 and i%b==0):
+            continue
+        if(i%a==0 or i%b==0):
+            c+=1
+        if(c>=k):
+            print("Win")
+            break
+    else:
+        print("Lose")
