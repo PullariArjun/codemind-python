626d0a46d0850e64896b7303ab46aeaf7eb263ef
PullariArjun <98091330+PullariArjun@users.noreply.github.com>
2023-02-04 10:35:12 +0530

    n=int(input()) for i in range(1,n+1):     for j in range(n-i):         print(' ',end='')     for j in range(1,(n*2)-((n-i)*2)):         print(i,end='')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,5 +1,7 @@
 n=int(input())
 for i in range(1,n+1):
-    for j in range(1,i+1):
-        print(j,end="")
+    for j in range(n-i):
+        print(' ',end='')
+    for j in range(1,(n*2)-((n-i)*2)):
+        print(i,end='')
     print()
